27abe6eef53347b2da515d9f39dce2eea4d6957b
--global <--global>
2024-10-28 16:03:47 +0100

    added choosing the symbol

---
+++ scripts/choice.js
@@ -0,0 +1,3 @@
+function choose(c) {
+    window.location.href = `./game.html?symbol=${c}`;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,102 +1,114 @@
-const cells = document.querySelectorAll('[data-cell]');
+const cells = document.querySelectorAll("[data-cell]");
 let isOTurn = false;
 
-cells.forEach(cell => {
-  	cell.addEventListener('click', handleClick, { once: true });
+cells.forEach((cell) => {
+  cell.addEventListener("click", handleClick, { once: true });
 });
 
+const urlParams = new URLSearchParams(window.location.search);
+let userSymbol = urlParams.get("symbol") || "x";
+
+function end(w) {
+  cells.forEach((cell) => cell.removeEventListener("click", handleClick));
+  alert(["You won!", "You lost...", "It's a draw."][w]); // sigma
+}
+
 function handleClick(e) {
-  	const cell = e.target;
-  	const currentClass = isOTurn ? 'o' : 'x';
-  	cell.classList.add(currentClass);
-  	cell.textContent = currentClass.toUpperCase();
-  	isOTurn = !isOTurn;
-  	let boardState = getCurrentBoardState(cells);
-  
-	let w = winner(boardState)
+  const cell = e.target;
 
-  	if(w == "x"){
-  	  setTimeout(()=>{
-  	    alert("Wygrałeś!")
-  	  },500)
-  	}
-	
-  	else if(w == "o"){
-  	  setTimeout(()=>{
-  	    alert("Przegrałeś!")
-  	  },500)
-  	}
+  const currentClass = isOTurn ? "o" : "x";
+  isOTurn = !isOTurn;
 
-	console.log(terminal(boardState));
-	console.log(utility(boardState)); 
+  cell.classList.add(currentClass);
+  cell.textContent = currentClass.toUpperCase();
+
+  let boardState = getCurrentBoardState(cells);
+
+  let w = winner(boardState);
+
+  if (w === userSymbol) end(0);
+  else if (w !== "") end(1);
+
+  if (actions().length == 0) end(2);
 }
 
 function getCurrentBoardState(cells) {
-  	// Returns current board state as list of chars
-  	return Array.from(cells, cell => cell.textContent.toLowerCase() || '');
+  // Returns current board state as list of chars
+  return Array.from(cells, (cell) => cell.textContent.toLowerCase() || "");
 }
 
-function actions(boardState) {
-  	// Returns set of all possible actions (index) available on the board
+function actions() {
+  // Returns set of all possible actions (index) available on the board
 
-  	const possibleActions = [];
+  const possibleActions = [];
 
-  	cells.forEach((boardState, index) => {
-    	if (boardState[index] === '')
-      		possibleActions.push(index);
-	});
+  cells.forEach((boardState, index) => {
+    if (boardState.innerText == "") possibleActions.push(index);
+  });
 
-  	return possibleActions;
+  return possibleActions;
 }
 
 function result(boardState, action) {
-  	//Returns the board that results from making move (index) on the board
+  //Returns the board that results from making move (index) on the board
 
-  	const newState = [...boardState];
-  	newState[action] = isOTurn ? 'o' : 'x';
+  const newState = [...boardState];
+  newState[action] = isOTurn ? "o" : "x";
 
-  	return newState;
+  return newState;
 }
 
 function winner(boardState) {
-	// rows
-  	for (let i = 0; i < 3; i++) {
-    	if (boardState[i] === boardState[i + 3] && boardState[i + 3] === boardState[i + 6] && boardState[i] !== null)
-      		return boardState[i];
-  	}
+  // rows
+  for (let i = 0; i < 9; i+=3) {
+    if (
+      boardState[i] == boardState[i + 1] &&
+      boardState[i + 1] == boardState[i + 2] &&
+      boardState[i] != ""
+    )
+      return boardState[i];
+  }
 
-  	// cols
-  	for (let i = 0; i < 3; i++) {
-    	if (boardState[i] === boardState[i + 3] && boardState[i + 3] === boardState[i + 6] && boardState[i] !== null) 
-      		return boardState[i];
-  	}
+  // cols
+  for (let i = 0; i < 3; i++) {
+    if (
+      boardState[i] == boardState[i + 3] &&
+      boardState[i + 3] == boardState[i + 6] &&
+      boardState[i] != ""
+    )
+      return boardState[i];
+  }
 
-	// diagonals
-  	if (boardState[0] === boardState[4] && boardState[4] === boardState[8] && boardState[0] !== null)
-    	return boardState[0];
-  	if (boardState[2] === boardState[4] && boardState[4] === boardState[6] && (boardState[2] === 'x' || boardState[2] === 'o'))
-    	return boardState[2];
+  // diagonals
+  if (
+    boardState[0] == boardState[4] &&
+    boardState[4] == boardState[8] &&
+    boardState[0] != ""
+  )
+    return boardState[0];
+  if (
+    boardState[2] == boardState[4] &&
+    boardState[4] == boardState[6] &&
+    boardState[2] != ""
+  )
+    return boardState[2];
 
-	return null;
+  return "";
 }
 
 function terminal(boardState) {
-	// Returns true if game is over, false otherwise
+  // Returns true if game is over, false otherwise
 
-	if(winner(boardState) == null)
-    	return false;
-  	return true;
+  if (winner(boardState) == "") return false;
+  return true;
 }
 
 function utility(boardState) {
-	// Returns 1 if X has won the game, -1 if O has won, 0 otherwise
+  // Returns 1 if X has won the game, -1 if O has won, 0 otherwise
 
-	let w = winner(boardState);
+  let w = winner(boardState);
 
-	if (w == 'x')
-		return 1;
-  	else if (w == 'o')
-      	return -1;
-  	else
-    	return 0;
+  if (w == "x") return 1;
+  else if (w == "o") return -1;
+  else return 0;
 }
